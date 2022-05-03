aa6e0b86e25330997dc16e0a491167a11eeb30de
Mihai Pirvulet <mitapirvuet@gmail.com>
2022-05-03 16:37:19 +0300

    change express example to use https

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require("express");
+const app = express();
+const https = require("https");
+const fs = require("fs");
 
-app.get('/', (req, res) => {
-  res.send('Deploy test!')
+const port = 3000;
+
+app.use(requireHTTPS);
+
+https.createServer({
+    key: fs.readFileSync("/etc/ssl/private/cloudflare_prvkey_lsacbucuresti.ro.pem"),
+    cert: fs.readFileSync("/etc/ssl/certs/cloudflare_lsacbucuresti.ro.pem")}, app)
+  .listen(port, function () {	
+      console.log(`[server.js] Server running fine on ${port}!`);
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+function requireHTTPS(req, res, next) {
+  if (!req.secure) {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+app.enable('trust proxy');
+
+app.use(function(req, res, next) {
+  if (req.secure){
+      return next();
+  }
+  res.redirect("https://" + req.headers.host + req.url);
+});
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
 })
